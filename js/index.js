2682d7c8b891cdf65fc5bb92b950c52d79001ba6
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-09-30 15:44:44 -0600

    copyright added

--- js/index.js
+++ js/index.js
@@ -80,8 +80,11 @@ p[2].textContent = siteContent["main-content"]["services-content"]
 p[3].textContent = siteContent["main-content"]["product-content"]
 p[4].textContent = siteContent["main-content"]["vision-content"]
 
-//contact
+//contact footer
 h4[5].textContent = siteContent["contact"]["contact-h4"]
 p[5].textContent = siteContent["contact"]["address"]
 p[6].textContent = siteContent["contact"]["phone"]
 p[7].textContent = siteContent["contact"]["email"]
+
+//copyright
+p[8].textContent = siteContent["footer"]["copyright"]
